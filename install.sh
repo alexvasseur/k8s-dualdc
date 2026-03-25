dd4c45dc5df360a5b37cea6e9e568321c1779e9d
Alexandre Vasseur <avasseur@gmail.com>
2026-03-25 20:43:16 +0100

    changed worker and defaults

--- install.sh
+++ install.sh
@@ -8,4 +8,4 @@ VERSION=v8.0.10-23
 
 kubectl apply -f https://raw.githubusercontent.com/RedisLabs/redis-enterprise-k8s-docs/$VERSION/bundle.yaml
 
-
+kubectl apply -f redis-operator-rbac.yaml
--- labels.sh
+++ labels.sh
@@ -3,11 +3,17 @@
 kubectl label node c1-control-plane topology.kubernetes.io/zone=quorum dc=quorum --overwrite
 
 kubectl label node c1-worker  topology.kubernetes.io/zone=dc1 dc=dc1 --overwrite
-kubectl label node c1-worker2 topology.kubernetes.io/zone=dc2 dc=dc2 --overwrite
+kubectl label node c1-worker2 topology.kubernetes.io/zone=dc1 dc=dc1 --overwrite
 kubectl label node c1-worker3 topology.kubernetes.io/zone=dc1 dc=dc1 --overwrite
+
 kubectl label node c1-worker4 topology.kubernetes.io/zone=dc2 dc=dc2 --overwrite
-kubectl label node c1-worker5 topology.kubernetes.io/zone=dc1 dc=dc1 --overwrite
+kubectl label node c1-worker5 topology.kubernetes.io/zone=dc2 dc=dc2 --overwrite
 kubectl label node c1-worker6 topology.kubernetes.io/zone=dc2 dc=dc2 --overwrite
 
-#kubectl label node c1-worker3 topology.kubernetes.io/zone=dc2 dc=dc2 dedicated=isolated --overwrite
-#kubectl taint node c1-worker3 dedicated=isolated:NoSchedule --overwrite
+kubectl label node c1-worker7 topology.kubernetes.io/zone=dcq dc=dcq --overwrite
+kubectl label node c1-worker8 topology.kubernetes.io/zone=dcq dc=dcq --overwrite
+kubectl label node c1-worker9 topology.kubernetes.io/zone=dcq dc=dcq --overwrite
+
+kubectl taint node c1-worker7 redis-dc=dcq:NoSchedule --overwrite
+kubectl taint node c1-worker8 redis-dc=dcq:NoSchedule --overwrite
+kubectl taint node c1-worker9 redis-dc=dcq:NoSchedule --overwrite
